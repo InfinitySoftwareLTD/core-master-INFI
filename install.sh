4cda7bbd52397009da6b1258836b6757367c6cc0
Infinity Software Ltd <31319908+Plusid@users.noreply.github.com>
2021-07-05 16:04:44 +0400

    without-rate

--- install.sh
+++ install.sh
@@ -214,17 +214,17 @@ alias ark="$HOME/core-bridgechain/packages/core/bin/run"
 echo 'alias infinity="$HOME/core-bridgechain/packages/core/bin/run"' >> ~/.bashrc
 
 rm -rf "$HOME/core-bridgechain"
-git clone "https://github.com/InfinitySoftwareLTD/core-master-INFI" "$HOME/core-bridgechain" || FAILED="Y"
+git clone -b without-rate "https://github.com/InfinitySoftwareLTD/core-master-INFI" "$HOME/core-bridgechain" || FAILED="Y"
 if [ "$FAILED" == "Y" ]; then
-    echo "Failed to fetch core repo with origin 'https://github.com/InfinitySoftwareLTD/core-master-INFI'"
+    echo "Failed to fetch core repo with origin 'https://github.com/InfinitySoftwareLTD/core-master-INFI/without-rate'"
 
     exit 1
 fi
 
 cd "$HOME/core-bridgechain"
-HAS_REMOTE=$(git branch -a | fgrep -o "remotes/origin/chore/bridgechain-changes")
+HAS_REMOTE=$(git branch -a | fgrep -o "remotes/origin/without-rate")
 if [ ! -z "$HAS_REMOTE" ]; then
-    git checkout chore/bridgechain-changes
+    git checkout chore/without-rate
 fi
 
 YARN_SETUP="N"
